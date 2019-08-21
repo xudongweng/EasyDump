66b03ccac90030dd32fd420b99e4b37af267972d
xudongweng <53887372+xudongweng@users.noreply.github.com>
2019-08-21 17:59:40 +0800

    2019/8/21

--- EasyDumpServer/src/main/java/com/xudong/Test.java
+++ EasyDumpServer/src/main/java/com/xudong/Test.java
@@ -9,6 +9,9 @@ import com.xudong.easydumpserver.helper.MySQLHelper;
 import java.io.File;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import org.apache.log4j.Logger;
+
+
 
 /**
  *
@@ -16,11 +19,22 @@ import java.util.ResourceBundle;
  */
 public class Test {
     public static void main(String[] args){
-        //System.out.println(MySQLHelper.getConnection("root", "123456", "localhost", "3306"));
+        
+        
+        Logger log=Logger.getLogger(Test.class);
+        log.trace("我是logger1，trace");
+        log.debug("我是logger1，debug");
+        log.info("我是logger1，info");
+        log.warn("我是logger1，warn");
+        log.error("我是logger1，error");
+        log.fatal("我是logger1，fatal");
         
         //System.out.println(Test.class.getResource(""));
         Locale myLocale = Locale.getDefault(Locale.Category.FORMAT);
         ResourceBundle rb = ResourceBundle.getBundle("config",myLocale);
         System.out.println(rb.getString("mysql.server"));
+        MySQLHelper mysqlcom=new MySQLHelper();
+        mysqlcom.setURL(rb.getString("mysql.server"),rb.getString("mysql.port"));
+        System.out.println(mysqlcom.getConnection(rb.getString("mysql.user"), rb.getString("mysql.password")));
     }
 }
--- EasyDumpServer/src/main/java/com/xudong/easydumpserver/helper/MySQLHelper.java
+++ EasyDumpServer/src/main/java/com/xudong/easydumpserver/helper/MySQLHelper.java
@@ -9,31 +9,78 @@ package com.xudong.easydumpserver.helper;
  *
  * @author sheriff
  */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.RowSet;
+import org.apache.log4j.Logger;
 
 public class MySQLHelper {
-    private final static String driver="com.mysql.cj.jdbc.Driver";
-    private final static String urlhead="jdbc:mysql://";
-    private final static String urltail="?serverTimezone=UTC";
+    private String driver="com.mysql.cj.jdbc.Driver";
+    private String urlhead="jdbc:mysql://";
+    private String urltail="?serverTimezone=UTC";
+    private String url;
+    private String user;
+    private String password;
+    private Logger log;
     
-    public static byte getConnection(String user,String password,String server,String port){
+    public MySQLHelper(){
+        this.log=Logger.getLogger(MySQLHelper.class);
+    }
+    
+    public void setURL(String user,String password,String server,String port){
+        this.url=this.urlhead+server+":"+port+this.urltail;
+        this.user=user;
+        this.password=password;
+    }
+    
+    public byte getConnection(){
+        String invokeinfo="getConnection():";
         try{
-            Class.forName(driver);
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306?serverTimezone=UTC","root","123456");
+            Class.forName(this.driver);
+            Connection conn = DriverManager.getConnection(this.url,this.user,this.password);
             if(!conn.isClosed()){
                 conn.close();
                 return 1;
             }else
-                return 0;
-        } catch(ClassNotFoundException e) {   
-            e.printStackTrace();   
+                return -1;
+        } catch(ClassNotFoundException e) {
+            log.error(invokeinfo + e.toString());
         } catch(SQLException e) {
-            e.printStackTrace();  
+            log.error(invokeinfo + e.toString());
         }catch (Exception e) {
-            e.printStackTrace();
+            log.error(invokeinfo + e.toString());
         }
-        return 0;
+        return -1;
+    }
+    
+    public RowSet getAllDB(){
+        String invokeinfo="getAllDB():";
+        try{
+            Class.forName(this.driver);
+            Connection conn = DriverManager.getConnection(this.url,this.user,this.password);
+            if(!conn.isClosed()){
+                Statement stmt = conn.createStatement(); //创建语句对象，用以执行sql语言
+                ResultSet rs = stmt.executeQuery("show databases"); 
+                CachedRowSet rowset = new CachedRowSetImpl();
+                rowset.populate(rs);
+                rs.close();
+                stmt.close();
+                conn.close();
+                return rowset;
+            }else
+                return null;
+            
+        } catch(ClassNotFoundException e) {
+            log.error(invokeinfo + e.toString());
+        } catch(SQLException e) {
+            log.error(invokeinfo + e.toString());
+        }catch (Exception e) {
+            log.error(invokeinfo + e.toString());
+        }
+        return null;
     }
 }
