7b5c98901e9771ca637914c3c9c657bc53cc9f84
xudongweng <527924966@qq.com>
2019-08-28 21:41:12 +0800

    2019-8-28 21:41

--- EasyDumpServer/src/main/java/easydump.sql
+++ EasyDumpServer/src/main/java/easydump.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 80012
+Source Server Version : 80016
 Source Host           : localhost:3306
 Source Database       : easydump
 
 Target Server Type    : MYSQL
-Target Server Version : 80012
+Target Server Version : 80016
 File Encoding         : 65001
 
-Date: 2019-08-28 17:12:58
+Date: 2019-08-28 21:40:23
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,11 +24,15 @@ CREATE TABLE `backuptables` (
   `databaseid` mediumint(4) unsigned DEFAULT NULL,
   `tablename` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of backuptables
 -- ----------------------------
+INSERT INTO `backuptables` VALUES ('1', '1', 'global_grants');
+INSERT INTO `backuptables` VALUES ('2', '1', 'help_relation');
+INSERT INTO `backuptables` VALUES ('3', '2', 'data_locks');
+INSERT INTO `backuptables` VALUES ('4', '2', 'events_statements_summary_by_account_by_event_name');
 
 -- ----------------------------
 -- Table structure for databases
@@ -46,14 +50,15 @@ CREATE TABLE `databases` (
   `backuptable` smallint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0:不指定备份表,1:指定备份表',
   `igonretable` smallint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0:不指定忽略表,1:指定忽略表',
   `code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'utf8',
+  `enable` smallint(1) unsigned NOT NULL DEFAULT '1' COMMENT '1：表示开启备份 0：表示关闭备份',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of databases
 -- ----------------------------
-INSERT INTO `databases` VALUES ('1', 'localhost', 'mysql', 'root', '123456', '3306', 'backup', '0', '0', '0', 'utf8');
-INSERT INTO `databases` VALUES ('2', 'localhost', 'performance_schema', 'root', '123456', '3306', 'backup', '0', '0', '0', 'utf8');
+INSERT INTO `databases` VALUES ('1', 'localhost', 'mysql', 'root', '123456', '3306', 'backup', '0', '1', '0', 'utf8', '1');
+INSERT INTO `databases` VALUES ('2', 'localhost', 'performance_schema', 'root', '123456', '3306', 'backup', '0', '1', '0', 'utf8', '1');
 
 -- ----------------------------
 -- Table structure for ingoretables
