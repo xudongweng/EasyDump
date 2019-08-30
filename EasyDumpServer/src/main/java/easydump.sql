677414e38e60ac7f0ae37860bdaed6f8b917ce9a
xudongweng <527924966@qq.com>
2019-08-30 17:02:42 +0800

    2019-8-30 17:02

--- EasyDumpServer/src/main/java/easydump.sql
+++ EasyDumpServer/src/main/java/easydump.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 80016
+Source Server Version : 80012
 Source Host           : localhost:3306
 Source Database       : easydump
 
 Target Server Type    : MYSQL
-Target Server Version : 80016
+Target Server Version : 80012
 File Encoding         : 65001
 
-Date: 2019-08-28 21:40:23
+Date: 2019-08-30 17:01:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -48,8 +48,9 @@ CREATE TABLE `databases` (
   `backuppath` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'backup',
   `strategy` smallint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0:getLockDB,1:getUnlockDB,2:getOnlyStruct,3:getOnlyData,4:getSplitTables',
   `backuptable` smallint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0:不指定备份表,1:指定备份表',
-  `igonretable` smallint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0:不指定忽略表,1:指定忽略表',
+  `ignoretable` smallint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0:不指定忽略表,1:指定忽略表',
   `code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'utf8',
+  `backupcmd` enum('mysqlpump','mysqldump') NOT NULL DEFAULT 'mysqldump',
   `enable` smallint(1) unsigned NOT NULL DEFAULT '1' COMMENT '1：表示开启备份 0：表示关闭备份',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -57,14 +58,14 @@ CREATE TABLE `databases` (
 -- ----------------------------
 -- Records of databases
 -- ----------------------------
-INSERT INTO `databases` VALUES ('1', 'localhost', 'mysql', 'root', '123456', '3306', 'backup', '0', '1', '0', 'utf8', '1');
-INSERT INTO `databases` VALUES ('2', 'localhost', 'performance_schema', 'root', '123456', '3306', 'backup', '0', '1', '0', 'utf8', '1');
+INSERT INTO `databases` VALUES ('1', 'localhost', 'mysql', 'root', '123456', '3306', 'backup', '4', '1', '0', 'utf8', 'mysqldump', '1');
+INSERT INTO `databases` VALUES ('2', 'localhost', 'performance_schema', 'root', '123456', '3306', 'backup', '3', '0', '1', 'utf8', 'mysqldump', '1');
 
 -- ----------------------------
--- Table structure for ingoretables
+-- Table structure for ignoretables
 -- ----------------------------
-DROP TABLE IF EXISTS `ingoretables`;
-CREATE TABLE `ingoretables` (
+DROP TABLE IF EXISTS `ignoretables`;
+CREATE TABLE `ignoretables` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `databaseid` mediumint(4) unsigned DEFAULT NULL,
   `tablename` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
@@ -72,9 +73,9 @@ CREATE TABLE `ingoretables` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
--- Records of ingoretables
+-- Records of ignoretables
 -- ----------------------------
-INSERT INTO `ingoretables` VALUES ('1', '1', 'global_grants');
-INSERT INTO `ingoretables` VALUES ('2', '1', 'help_relation');
-INSERT INTO `ingoretables` VALUES ('3', '2', 'data_locks');
-INSERT INTO `ingoretables` VALUES ('4', '2', 'events_statements_summary_by_account_by_event_name');
+INSERT INTO `ignoretables` VALUES ('1', '1', 'global_grants');
+INSERT INTO `ignoretables` VALUES ('2', '1', 'help_relation');
+INSERT INTO `ignoretables` VALUES ('3', '2', 'data_locks');
+INSERT INTO `ignoretables` VALUES ('4', '2', 'events_statements_summary_by_account_by_event_name');
