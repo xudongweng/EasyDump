615221866a576fc1b2b0d1a6ae73f0345af4375b
xudongweng <527924966@qq.com>
2019-09-03 16:30:44 +0800

    2019-9-3 16:30

--- EasyDumpServer/src/main/java/com/easydumpserver/Test.java
+++ EasyDumpServer/src/main/java/com/easydumpserver/Test.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -23,6 +24,7 @@ import java.util.zip.ZipOutputStream;
  */
 public class Test {
     public static void main(String[] args) throws IOException{
+        
         MySQLDumpStringController a=new MySQLDumpStringController();
         a.setDumpBaseInfo();
         a.setDumpString();
@@ -33,22 +35,26 @@ public class Test {
         List dumpPathList=dao.getDumpPath();
         int i=dumpList.size();
         ZipUtilsHelper zuh=new ZipUtilsHelper();
+        
         while(--i>=0){
-            InputStream in = Runtime.getRuntime().exec("").getInputStream();
+            //InputStream in = Runtime.getRuntime().exec(dumpList.get(i).toString()).getInputStream();
             //zuh.zipStreamCompress(in, destPath, filename);
+            System.out.println(dumpPathList.get(i));
         }
+        
+
         /*
-        byte b[] = new byte[1024];  
+        byte b[] = new byte[1024];
         ZipOutputStream zout = null;
         
         zout = new ZipOutputStream(new FileOutputStream("D:\\e.zip"));
         ZipEntry e = new ZipEntry("e.sql");
         zout.putNextEntry(e); 
-        InputStream in = Runtime.getRuntime().exec("E:\\MySQL\\bin\\mysqldump  --default-character-set=utf8 --single-transaction -h192.168.3.70 --user=sa --password=123456 --port=3306 sbtest").getInputStream();
+        InputStream in = Runtime.getRuntime().exec("D:\\MySQL\\bin\\mysqldump  --default-character-set=utf8 --single-transaction -hlocalhost --user=root --password=123456 --port=3306 mysql").getInputStream();
         
         int len = 0;  
         while ((len = in.read(b)) != -1) {
-            System.out.println(len);
+            //System.out.println(len);
             zout.write(b, 0, len);  
         }  
 
@@ -58,5 +64,19 @@ public class Test {
 
         in.close();
         */
+        /*
+        byte b[] = new byte[4096*1024];
+        GZIPOutputStream gos=new GZIPOutputStream(new FileOutputStream("D:\\e.gz"));
+        InputStream in = Runtime.getRuntime().exec("D:\\MySQL\\bin\\mysqldump  --default-character-set=utf8 --single-transaction -hlocalhost --user=root --password=123456 --port=3306 mysql").getInputStream();
+        int len = 0;  
+        while ((len = in.read(b)) != -1) {
+            //System.out.println(len);
+            //zout.write(b, 0, len);
+            gos.write(b, 0, len);
+        }
+        gos.finish();
+        gos.flush();
+        gos.close();
+        in.close();*/
     }
 }
--- EasyDumpServer/src/main/java/com/easydumpserver/mysql/helper/compress/ZipUtilsHelper.java
+++ EasyDumpServer/src/main/java/com/easydumpserver/mysql/helper/compress/ZipUtilsHelper.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 import org.apache.log4j.Logger;
@@ -18,34 +19,53 @@ import org.apache.log4j.Logger;
  * @author sheriff
  */
 public class ZipUtilsHelper {
-    private Logger log;
+    private Logger log=null;
     public ZipUtilsHelper(){
         this.log=Logger.getLogger(ZipUtilsHelper.class);
     }
     
-    public void zipStreamCompress(InputStream in,String destPath,String filename){
-        try{
-        ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(destPath+filename+".zip"));
-        ZipEntry e = new ZipEntry(filename);
-        zout.putNextEntry(e);
-        byte b[] = new byte[1024];
-        int len = 0;  
-        while ((len = in.read(b)) != -1) {
-            //System.out.println(len);
-            zout.write(b, 0, len);  
-        }  
-
-        zout.closeEntry();  
-        zout.flush();  
-        zout.close();  
-
-        in.close();
-        }catch(FileNotFoundException e){
-            log.error(e.toString());
-        }catch(IOException e){
-            log.error(e.toString());
-        }catch(Exception e){
-            log.error(e.toString());
-        }
+    public void zipStreamCompress(InputStream in,String destPath,String filename,String zipname){
+         try{
+             ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(destPath+zipname+".zip"));
+             ZipEntry e = new ZipEntry(filename);
+             zout.putNextEntry(e);
+             byte buff[] = new byte[1024];
+             int len = 0;  
+             while ((len = in.read(buff)) != -1) {
+                 zout.write(buff, 0, len);
+             }
+             zout.closeEntry();  
+             zout.flush();  
+             zout.close();  
+             in.close();
+         }catch(FileNotFoundException e){
+             log.error(e.toString());
+         }catch(IOException e){
+             log.error(e.toString());
+         }catch(Exception e){
+             log.error(e.toString());
+         }
+    }
+    
+    public void gzipStreamCompress(InputStream in,String destPath,String filename,String zipname){
+         try{
+             GZIPOutputStream gos=new GZIPOutputStream(new FileOutputStream(destPath+zipname+".gz"));
+             byte buff[] = new byte[1024];
+             int len = 0;  
+             while ((len = in.read(buff)) != -1) {
+                 //System.out.println(len);
+                 gos.write(buff, 0, len);
+             }  
+             gos.finish();
+             gos.flush();
+             gos.close();
+             in.close();
+         }catch(FileNotFoundException e){
+             log.error(e.toString());
+         }catch(IOException e){
+             log.error(e.toString());
+         }catch(Exception e){
+             log.error(e.toString());
+         }
     }
 }
